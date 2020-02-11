baf050045f09909984df496cf6d63edfeb224f75
Kindritskiy Maksym <kindritskiy.m@gmail.com>
2020-02-11 14:56:55 +0200

    when pased kv flag with json value, escape in single quoutes (#8)
    * when pased kv flag with json value, escape in single quoutes
    
    * check for nil in cmd list

--- commands/command/cmd.go
+++ commands/command/cmd.go
@@ -1,9 +1,30 @@
 package command
 
 import (
+	"fmt"
 	"os"
 	"strings"
 )
+
+func stringPartition(s, sep string) (string, string, string) {
+	sepPos := strings.Index(s, sep)
+	if sepPos == -1 { // no seperator found
+		return s, "", ""
+	}
+	split := strings.SplitN(s, sep, 2)
+	return split[0], sep, split[1]
+}
+
+// e.g if value is a json --key='{"value": 1}'
+// it will wrap value in '' - --key=''{"value": 1}''
+// and when escaped it become --key='{"value": 1}'
+func escapeFlagValue(str string) string {
+	if strings.Contains(str, "=") {
+		key, sep, val := stringPartition(str, "=")
+		str = strings.Join([]string{key, fmt.Sprintf("'%s'", val)}, sep)
+	}
+	return str
+}
 
 func parseAndValidateCmd(cmd interface{}, newCmd *Command) error {
 	switch cmd := cmd.(type) {
@@ -12,11 +33,23 @@ func parseAndValidateCmd(cmd interface{}, newCmd *Command) error {
 	case []interface{}:
 		cmdList := make([]string, len(cmd))
 		for _, v := range cmd {
-			cmdList = append(cmdList, v.(string))
+			if v == nil {
+				return newCommandError(
+					"got nil in cmd list",
+					newCmd.Name,
+					CMD,
+					"",
+				)
+			}
+			cmdList = append(cmdList, fmt.Sprintf("%s", v))
 		}
 		// cut binary path and command name
 		cmdList = append(cmdList, os.Args[2:]...)
-		newCmd.Cmd = strings.TrimSpace(strings.Join(cmdList, " "))
+		var escapedCmdList []string
+		for _, val := range cmdList {
+			escapedCmdList = append(escapedCmdList, escapeFlagValue(val))
+		}
+		newCmd.Cmd = strings.TrimSpace(strings.Join(escapedCmdList, " "))
 	default:
 		return newCommandError(
 			"must be either string or list of string",
