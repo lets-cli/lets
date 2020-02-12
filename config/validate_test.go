b881b1b7d11f84968712ea4726d42918438816ad
Kindritskiy Max <kindritskiy.m@gmail.com>
2020-02-12 09:06:34 +0200

    add tests for circular deps validation

---
+++ config/validate_test.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"github.com/kindritskyiMax/lets/commands/command"
+	"testing"
+)
+
+func TestValidateCircularDeps(t *testing.T) {
+	t.Run("command skip itself", func(t *testing.T) {
+
+		testCfg := &Config{
+			Commands: make(map[string]command.Command),
+		}
+		testCfg.Commands["a-cmd"] = command.Command{
+			Name:    "a-cmd",
+			Depends: []string{"noop"},
+		}
+		testCfg.Commands["b-cmd"] = command.Command{
+			Name:    "b-cmd",
+			Depends: []string{"noop"},
+		}
+		err := validateCircularDepends(testCfg)
+
+		if err != nil {
+			t.Errorf("checked itself when validation circular depends. got:  %s", err)
+		}
+	})
+
+	t.Run("command with similar name should not fail validation", func(t *testing.T) {
+
+		testCfg := &Config{
+			Commands: make(map[string]command.Command),
+		}
+		testCfg.Commands["a-cmd"] = command.Command{
+			Name:    "a-cmd",
+			Depends: []string{"b1-cmd"},
+		}
+		testCfg.Commands["b"] = command.Command{
+			Name:    "b",
+			Depends: []string{"a-cmd"},
+		}
+		testCfg.Commands["b1-cmd"] = command.Command{
+			Name:    "b1-cmd",
+			Depends: []string{"noop"},
+		}
+		err := validateCircularDepends(testCfg)
+
+		if err != nil {
+			t.Errorf("checked itself when validation circular depends. got:  %s", err)
+		}
+	})
+
+	t.Run("validation should fail", func(t *testing.T) {
+
+		testCfg := &Config{
+			Commands: make(map[string]command.Command),
+		}
+		testCfg.Commands["a-cmd"] = command.Command{
+			Name:    "a-cmd",
+			Depends: []string{"b1-cmd"},
+		}
+		testCfg.Commands["b"] = command.Command{
+			Name:    "b",
+			Depends: []string{"a-cmd"},
+		}
+		testCfg.Commands["b1-cmd"] = command.Command{
+			Name:    "b1-cmd",
+			Depends: []string{"a-cmd"},
+		}
+		err := validateCircularDepends(testCfg)
+
+		if err == nil {
+			t.Errorf("validation should fail. got: %s", err)
+		}
+	})
+}
