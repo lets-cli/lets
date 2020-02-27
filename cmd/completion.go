39c4f1897c7f14a86bb1584e293472de4d3ce20a
Kindritskiy Max <kindritskiy.m@gmail.com>
2020-02-27 23:55:30 +0200

    add completion command

---
+++ cmd/completion.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"io"
+	"text/template"
+)
+
+var tmpl = `To enable completion in your shell, run:
+  eval "$(lets completion -s <shell>)"
+You can add that to your '~/.bash_profile' to enable completion whenever you
+start a new shell.
+`
+
+var (
+	zshCompletionText = `
+#compdef lets
+function _lets {
+  local -a commands
+
+  _arguments -C \
+    "1: :->cmnds" \
+    "*::arg:->args"
+
+  case $state in
+  cmnds)
+    commands=({{range .Commands}}{{if not .Hidden}}
+	{{if .Short}}"{{.Name}}:{{.Short}}"{{else}}"{{.Name}}:No description for command {{.Name}}"{{end}}{{end}}{{end}}
+    )
+    _describe "command" commands
+    ;;
+  esac
+}
+
+_lets
+`
+)
+
+func genZshCompletion(rootCmd *cobra.Command, w io.Writer) error {
+	tmpl, err := template.New("Main").Parse(zshCompletionText)
+	if err != nil {
+		return fmt.Errorf("error creating zsh completion template: %v", err)
+	}
+	return tmpl.Execute(w, rootCmd)
+}
+
+func initCompletionCmd(rootCmd *cobra.Command) {
+	var completionCmd = &cobra.Command{
+		Use:    "completion",
+		Hidden: true,
+		Short:  "Generates completion scripts",
+		Long:   tmpl,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			shellType, err := cmd.Flags().GetString("shell")
+			if err != nil {
+				return err
+			}
+
+			switch shellType {
+			case "bash":
+				return rootCmd.GenBashCompletion(cmd.OutOrStdout())
+			case "zsh":
+				return genZshCompletion(rootCmd, cmd.OutOrStdout())
+			default:
+				return fmt.Errorf("unsupported shell type %q", shellType)
+			}
+		},
+	}
+	rootCmd.AddCommand(completionCmd)
+	completionCmd.Flags().StringP("shell", "s", "bash", "The type of shell")
+}
