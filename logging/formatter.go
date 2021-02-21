22b72f4664d5446c0ce13df18d08c338e27c501b
Max Kindritskiy <kindritskiy.m@gmail.com>
2021-02-21 18:56:11 +0200

    drop colors formatting in logger

--- logging/formatter.go
+++ logging/formatter.go
@@ -6,14 +6,6 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
-
-	"github.com/lets-cli/lets/env"
-)
-
-const (
-	red    = 31
-	yellow = 33
-	gray   = 90
 )
 
 // LogRepresenter is an interface for objects that can format themselves for
@@ -28,33 +20,11 @@ type Formatter struct{}
 // Format implements the log.Formatter interface
 func (f *Formatter) Format(entry *log.Entry) ([]byte, error) {
 	buff := &bytes.Buffer{}
-	buff.WriteString(writeLevel(entry.Level))
 	buff.WriteString(writeData(entry.Data))
 	buff.WriteString(entry.Message)
 	buff.WriteString("\n")
 
 	return buff.Bytes(), nil
-}
-
-func withColor(color int, msg string) string {
-	if env.IsNotColorOutput() {
-		return msg
-	}
-
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color, msg)
-}
-
-func writeLevel(level log.Level) string {
-	switch level {
-	case log.DebugLevel:
-		return fmt.Sprintf("[%s] ", withColor(gray, "DEBUG"))
-	case log.WarnLevel:
-		return fmt.Sprintf("[%s] ", withColor(yellow, "WARN"))
-	case log.ErrorLevel, log.FatalLevel, log.PanicLevel:
-		return fmt.Sprintf("[%s] ", withColor(red, "ERROR"))
-	default:
-		return ""
-	}
 }
 
 func writeData(fields log.Fields) string {
